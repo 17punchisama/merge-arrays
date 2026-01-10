60e4e19475b63c50901cb0686edbb135c598a1d9
17punchisama <punxo9lass22@gmail.com>
2026-01-10 23:05:09 +0700

    add jest report

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,18 @@
 module.exports = {
-  transform: {'^.+\\.ts?$': 'ts-jest'},
-  testEnvironment: 'node',
-  testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  transform: { "^.+\\.(ts|tsx)$": "ts-jest" },
+  testEnvironment: "node",
+  testMatch: ["**/test/**/*.test.ts"],
+  moduleFileExtensions: ["ts", "tsx", "js", "json"],
+  reporters: [
+    "default",
+    [
+      "jest-html-reporter",
+      {
+        pageTitle: "Merge Test Report",
+        outputPath: "report.html",
+        includeFailureMsg: true,
+        includeConsoleLog: true
+      }
+    ]
+  ]
 };
